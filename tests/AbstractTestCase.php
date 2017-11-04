862759e537d64423ad6da5d7c9e4c3db5ed771eb
Filip Horvat <fico7489@gmail.com>
2017-11-04 10:51:01 +0100

    add .gitignore

---
+++ tests/AbstractTestCase.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests;
+
+use Mockery;
+use PHPUnit_Framework_TestCase;
+
+abstract class AbstractTestCase extends PHPUnit_Framework_TestCase
+{
+    public function tearDown()
+    {
+        Mockery::close();
+
+        parent::tearDown();
+    }
+
+    public static function test(){
+        echo 'test';
+    }
+}
---
+++ tests/PivotEventTraitTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests;
+
+use Mockery;
+
+class PivotEventTraitTest extends AbstractTestCase
+{
+    public function test_restored_handler_makes_model_searchable()
+    {
+        $this->assertTrue(true);
+    }
+
+    public static function test(){
+        echo 'test';
+    }
+}
