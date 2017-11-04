3e4bf1d9f57117c5507dfe8a2f625ca2d0cf8f45
Filip Horvat <fico7489@gmail.com>
2017-11-04 17:19:02 +0100

    add methods for events

--- src/Traits/PivotEventTrait.php
+++ src/Traits/PivotEventTrait.php
@@ -59,15 +59,6 @@ trait PivotEventTrait
      */
     public function fireModelEvent($event, $halt = true, $relationName = null)
     {
-        $pivotEvents = [
-            'pivotAttaching',
-            'pivotAttached',
-            'pivotDetaching',
-            'pivotDetached',
-            'pivotUpdating',
-            'pivotUpdated',
-        ];
-
         if (! isset(static::$dispatcher)) {
             return true;
         }
@@ -82,5 +73,56 @@ trait PivotEventTrait
         $payload = ['model' => $this, 'relation' => $relationName];
 
         return static::$dispatcher->$method($event, $payload);
+    }
+
+
+    public static function pivotAttaching($callback, $priority = 0)
+    {
+        static::registerModelEvent('pivotAttaching', $callback, $priority);
+    }
+
+    public static function pivotAttached($callback, $priority = 0)
+    {
+        static::registerModelEvent('pivotAttached', $callback, $priority);
+    }
+
+    public static function pivotDetaching($callback, $priority = 0)
+    {
+        static::registerModelEvent('pivotDetaching', $callback, $priority);
+    }
+
+    public static function pivotDetached($callback, $priority = 0)
+    {
+        static::registerModelEvent('pivotDetached', $callback, $priority);
+    }
+
+    public static function pivotUpdating($callback, $priority = 0)
+    {
+        static::registerModelEvent('pivotUpdating', $callback, $priority);
+    }
+
+    public static function pivotUpdated($callback, $priority = 0)
+    {
+        static::registerModelEvent('pivotUpdated', $callback, $priority);
+    }
+
+    /**
+     * Get the observable event names.
+     *
+     * @return array
+     */
+    public function getObservableEvents()
+    {
+        return array_merge(
+            [
+                'creating', 'created', 'updating', 'updated',
+                'deleting', 'deleted', 'saving', 'saved',
+                'restoring', 'restored',
+                'pivotAttaching', 'pivotAttached',
+                'pivotDetaching', 'pivotDetached',
+                'pivotUpdating', 'pivotUpdated',
+            ],
+            $this->observables
+        );
     }
 }
--- tests/Models/BaseModel.php
+++ tests/Models/BaseModel.php
@@ -7,5 +7,4 @@ use Illuminate\Database\Eloquent\Model;
 
 class BaseModel extends Model
 {
-    
 }
