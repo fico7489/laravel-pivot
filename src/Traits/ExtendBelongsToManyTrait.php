146dc800e0de2fc5ed004bf8c4648d2e433327c3
Filip Horvat <fico7489@gmail.com>
2017-12-09 08:29:00 +0100

    separate extending belongs to many relation & fire model event

--- src/Traits/ExtendBelongsToManyTrait.php
+++ src/Traits/ExtendBelongsToManyTrait.php
@@ -4,7 +4,7 @@ namespace Fico7489\Laravel\Pivot\Traits;
 
 use Fico7489\Laravel\Pivot\Relations\BelongsToManyCustom;
 
-trait BelongsToManyCustomTrait
+trait ExtendBelongsToManyTrait
 {
     /**
      * Define a many-to-many relationship.
--- src/Traits/PivotEventTrait.php
+++ src/Traits/PivotEventTrait.php
@@ -4,40 +4,8 @@ namespace Fico7489\Laravel\Pivot\Traits;
 
 trait PivotEventTrait
 {
-    use BelongsToManyCustomTrait;
-    
-    /**
-     * Fire the given event for the model.
-     *
-     * @param  string  $event
-     * @param  bool  $halt
-     * @return mixed
-     */
-    public function fireModelEvent($event, $halt = true, $relationName = null, $pivotIds = [], $pivotIdsAttributes = [])
-    {
-        if (! isset(static::$dispatcher)) {
-            return true;
-        }
-
-        // First, we will get the proper method to call on the event dispatcher, and then we
-        // will attempt to fire a custom, object based event for the given event. If that
-        // returns a result we can return that result, or we'll call the string events.
-        $method = $halt ? 'until' : 'fire';
-
-        $result = $this->filterModelEventResults(
-            $this->fireCustomModelEvent($event, $method)
-        );
-
-        if ($result === false) {
-            return false;
-        }
-        
-        $payload = ['model' => $this, 'relation' => $relationName, 'pivotIds' => $pivotIds, 'pivotIdsAttributes' => $pivotIdsAttributes];
-
-        return ! empty($result) ? $result : static::$dispatcher->{$method}(
-            "eloquent.{$event}: ".static::class, $payload
-        );
-    }
+    use ExtendBelongsToManyTrait;
+    use ExtendFireModelEventTrait;
 
     public static function pivotAttaching($callback, $priority = 0)
     {
