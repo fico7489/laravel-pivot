2e0d54693536a281f84873df8c2cec6a7f1e4f1c
Erik Gaal <me@erikgaal.nl>
2018-04-05 11:24:56 +0200

    Use index based event payload instead of associative array

--- tests/PivotEventTraitTest.php
+++ tests/PivotEventTraitTest.php
@@ -29,7 +29,7 @@ class PivotEventTraitTest extends TestCase
 
         \Event::listen('eloquent.*', function ($eventName, array $data) {
             if (strpos($eventName, 'eloquent.retrieved') !== 0) {
-                self::$events[] = ['name' => $eventName, 'model' => $data['model'], 'relation' => $data['relation'], 'pivotIds' => $data['pivotIds'], 'pivotIdsAttributes' => $data['pivotIdsAttributes']];
+                self::$events[] = array_merge([$eventName], $data);
             }
         });
     }
@@ -307,7 +307,7 @@ class PivotEventTraitTest extends TestCase
     {
         $i = 0;
         foreach ($events as $event) {
-            $this->assertEquals(self::$events[$i]['name'], $event);
+            $this->assertEquals(self::$events[$i][0], $event);
             $i++;
         }
         $this->assertEquals(count($events), count(self::$events));
@@ -315,9 +315,9 @@ class PivotEventTraitTest extends TestCase
 
     private function check_variables($number, $ids, $idsAttributes = [], $relation = 'roles')
     {
-        $this->assertEquals(self::$events[$number]['pivotIds'], $ids);
-        $this->assertEquals(self::$events[$number]['pivotIdsAttributes'], $idsAttributes);
-        $this->assertEquals(self::$events[$number]['relation'], $relation);
+        $this->assertEquals(self::$events[$number][2], $relation);
+        $this->assertEquals(self::$events[$number][3], $ids);
+        $this->assertEquals(self::$events[$number][4], $idsAttributes);
     }
 
     private function check_database($count, $value, $number = 0, $attribute = 'value', $table = 'role_user')
